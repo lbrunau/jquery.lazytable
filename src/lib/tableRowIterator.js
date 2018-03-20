81786baf5002e8336f3ec5605afb0942cac17e51
Leonhard Brunauer <leonhard.brunauer@gmx.at>
2018-03-20 10:24:48 +0100

    fixed tableRowIterator: removed closure in favor of direct calls to TableRowIterator.prototype

--- src/lib/tableRowIterator.js
+++ src/lib/tableRowIterator.js
@@ -3,37 +3,35 @@ export default function TableRowIterator(generatorFunction, rows) {
 	this.rows = rows;
 	this.current = 0;
 }
-(function() {
-	this.clone = function() {
-		var newIt = new TableRowIterator(this.generatorFunction);
-		newIt.rows = this.rows;
-		newIt.current = this.current;
-		return newIt;
-	};
+TableRowIterator.prototype.clone = function() {
+	var newIt = new TableRowIterator(this.generatorFunction);
+	newIt.rows = this.rows;
+	newIt.current = this.current;
+	return newIt;
+};
+TableRowIterator.prototype.getCurrent = function() {
+	return this.current;
+};
+
+TableRowIterator.prototype.setCurrent = function(index) {
+	if(index >= 0 && index < this.rows.length) {
+		this.current = index;
+	}
+};
+
+TableRowIterator.prototype.next = function() {
+	return this.generatorFunction(this.rows[this.current++]);
+};
+
+TableRowIterator.prototype.hasNext = function() {
+	return this.current < this.rows.length;
+};
+
+TableRowIterator.prototype.prev = function() {
+	return this.generatorFunction(this.rows[--this.current]);
+};
+
+TableRowIterator.prototype.hasPrev = function() {
+	return this.current > 0;
+};
 	
-	this.getCurrent = function() {
-		return this.current;
-	};
-	
-	this.setCurrent = function(index) {
-		if(index >= 0 && index < this.rows.length) {
-			this.current = index;
-		}
-	};
-	
-	this.next = function() {
-		return this.generatorFunction(this.rows[this.current++]);
-	};
-	
-	this.hasNext = function() {
-		return this.current < this.rows.length;
-	};
-	
-	this.prev = function() {
-		return this.generatorFunction(this.rows[--this.current]);
-	};
-	
-	this.hasPrev = function() {
-		return this.current > 0;
-	};
-}).call(TableRowIterator.prototype);
