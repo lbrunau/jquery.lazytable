4f50209fabb75039a2476790cd7bbeaab663f18d
Leonhard Brunauer <leonhard.brunauer@gmx.at>
2018-03-27 09:38:37 +0200

    Use babel for production builds.

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,5 +6,10 @@ module.exports = merge(common, {
   output: {
     filename: '[name].min.js'
   },
-  mode: 'production'
+  mode: 'production',
+  module: {
+	  rules: [
+	    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+	  ]
+	}
 });
